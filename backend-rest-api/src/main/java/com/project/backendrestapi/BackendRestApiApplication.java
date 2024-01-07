e628f5013c6126cc1865d230f52ff2a196fd37f4
Krish Dholeria <krishdholeria1@gmail.com>
2024-01-07 17:11:19 +0530

    backend and frontend initialized

--- backend-rest-api/src/main/java/com/project/backendrestapi/BackendRestApiApplication.java
+++ backend-rest-api/src/main/java/com/project/backendrestapi/BackendRestApiApplication.java
@@ -1,13 +1,13 @@
-package com.project.onlinebankingsystem;
+package com.project.backendrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineBankingSystemApplication {
+public class BackendRestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlineBankingSystemApplication.class, args);
+		SpringApplication.run(BackendRestApiApplication.class, args);
 	}
 
 }

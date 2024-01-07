6f1e3047e437e45f5f8c3decd41b8184f0b96b96
Krish Dholeria <krishdholeria1@gmail.com>
2024-01-07 16:40:09 +0530

    spring initialized

---
+++ Backend/online-banking-system/src/main/java/com/project/onlinebankingsystem/OnlineBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.onlinebankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/online-banking-system/src/test/java/com/project/onlinebankingsystem/OnlineBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.onlinebankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

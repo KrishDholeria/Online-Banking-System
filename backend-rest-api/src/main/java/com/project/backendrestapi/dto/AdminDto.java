515032d5da20de5fa212fea53b5fc0293f170dac
Krish Dholeria <krishdholeria1@gmail.com>
2024-02-11 14:06:24 +0530

    cross origin setup

--- backend-rest-api/src/main/java/com/project/backendrestapi/dto/AdminDto.java
+++ backend-rest-api/src/main/java/com/project/backendrestapi/dto/AdminDto.java
@@ -13,10 +13,4 @@ public class AdminDto {
     private String userName;
     private String password;
     private Long personId;
-    private String lastName;
-    private String firstName;
-    private String dob;
-    private String email;
-    private String phoneNo;
-    private String address;
 }

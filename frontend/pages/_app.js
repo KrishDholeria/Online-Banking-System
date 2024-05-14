a114cae21f84a3619b4402aa1edc95af6f774de6
Krish Dholeria <krishdholeria1@gmail.com>
2024-05-14 22:15:07 +0530

    backend deployed

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { Toaster } from '@/components/ui/sonner';
 
 export default function App({ Component, pageProps }) {
-  axios.defaults.baseURL = "http://localhost:8080";
+  axios.defaults.baseURL = "http://bank4ever.hzdphta0czgrc8bw.centralindia.azurecontainer.io:8080";
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     const token = localStorage.getItem('customer-token');
